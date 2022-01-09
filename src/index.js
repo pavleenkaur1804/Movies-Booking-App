61d430535a0e383cc7b9faeb84c389ae6253b153
PavleenKaur <pavleen.kaur1804@gmail.com>
2022-01-09 16:19:37 +0530

    third commit(Upcoming/Released Movie Feature)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Home from './screens/home/Home';
 
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    
+    <Home />
   </React.StrictMode>,
   document.getElementById('root')
 );
